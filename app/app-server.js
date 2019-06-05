83fc4963f87c6a0462ecad7f9e0d9744bfc8a73d
Davide <daviderng@gmail.com>
2019-06-05 15:07:23 +0200

    bot command start added

--- app/app-server.js
+++ app/app-server.js
@@ -1,8 +1,6 @@
-// Load the databse, the http server, the websocekt server and the model
-//const db = require('./database');
-const db = require('./database');
+require('dotenv').config({ path: 'variables.prod.env' });
+
 const httpServer = require('./servers/http');
-const resources = require('./resources/model');
 
 // Require all the sensor plugins we need
 const fanPlugin = require('./plugins/fan');
@@ -10,11 +8,20 @@ const humPlugin = require('./plugins/humidity');
 const lampPlugin = require('./plugins/lamp');
 const tempPlugin = require('./plugins/temperature');
 
-fanPlugin.start({'simulate': false, 'frequency': 1000});
+fanPlugin.start({'simulate': true, 'frequency': 1000});
 humPlugin.start({'simulate': true, 'frequency': 1000});
-lampPlugin.start({'simulate': false, 'frequency': 1000});
+lampPlugin.start({'simulate': true, 'frequency': 1000});
 tempPlugin.start({'simulate': true, 'frequency': 1000});
  
-const server = httpServer.listen(resources.app.port, function () {
-    console.info('Your WoT Pi is up and running');
+const database = require('./database/database');
+database.startLogger({'frequency': 4000});
+
+const bot = require('./telegram/bot');
+bot.start({'run': false});
+
+
+httpServer.listen(process.env.port, function () {
+    console.info('Iot incubator started!!');
  });
+
+ 
