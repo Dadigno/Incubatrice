5b7f6f43175c559cb7ff8ea7855637f0581686e6
Davide <daviderng@gmail.com>
2019-06-05 15:12:04 +0200

    replaced

--- app/models/DataTypes.js
+++
@@ -1,9 +0,0 @@
-//elenco i campi "type" che potrò assegnare a ogni dato salvato nella collection
-const DataTypes = Object.freeze({
-    temperature: "temperature [C°]",
-    humidity: "humidity [%]",
-    fan: "fan velocity [%]",
-    lamp: "lamp [true, false]"
-});
-//esporto in modo da utilizzare i tipi definiti quando faccio una chiamata al database
-module.exports = DataTypes;
